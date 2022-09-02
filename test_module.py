f2615fe913748242dd40291e0bc27cc30a33c57d
youssef ben rejeb <youssef.benrejeb@gmail.com>
2022-09-02 12:39:53 +0100

    Five tests are ok

--- test_module.py
+++ test_module.py
@@ -5,7 +5,9 @@ from time_calculator import add_time
 
 @pytest.fixture()
 def input_valu():
-    valu = ["3:00 PM", "3:10", "Monday", "11:30 AM", "2:32", "Monday", "11:43 AM", "00:20"]
+    valu = ["3:00 PM", "3:10", "Monday", "11:30 AM", "2:32", "Monday", "11:43 AM", "00:20", "10:10 PM", "3:30", "11:43 "
+                                                                                                                "PM",
+            "24:20", "tueSday"]
     return valu
 
 
@@ -24,6 +26,16 @@ def test_add_time_2_day(input_valu):
     assert add_time(input_valu[3], input_valu[4], input_valu[5]) == "2 : 02 PM, Monday"
 
 
-#@pytest.mark.skip
+# @pytest.mark.skip
 def test_add_time_2(input_valu):
     assert add_time(input_valu[6], input_valu[7]) == "12 : 03 PM"
+
+
+# @pytest.mark.skip
+def test_add_time_3(input_valu):
+    assert add_time(input_valu[8], input_valu[9]) == "1 : 40 AM (next day)"
+
+
+@pytest.mark.skip
+def test_add_time_3_day(input_valu):
+    assert add_time(input_valu[10], input_valu[11], input_valu[12]) == "12:03 AM, Thursday (2 days later)"
--- time_calculator.py
+++ time_calculator.py
@@ -1,33 +1,44 @@
 def add_time(start, duration, day=None):
+    next_d = "(next day)"
+
     list_start_time = start.split()
     list_start = list_start_time[0].split(":")
     list_duration = duration.split(":")
     h = int(list_start[0]) + int(list_duration[0])
     ap = list_start_time[1]
-    if 12 < h < 24:
-        if h == 12:
-            h = 12
-        else:
-            h = h - 12
-
-        if ap == "AM":
-            ap = "PM"
-        else:
-            ap = "AM"
 
     m = int(list_start[1]) + int(list_duration[1])
     if m > 60:
         if m % 60 != 0:
             h = h + (m // 60)
             m = m % 60
+    if 12 < h < 24:
+        h = h - 12
 
+        if ap == "AM":
+            ap = "PM"
+        else:
+            ap = "AM"
+            re = str(h) + " : " + f'{m:02}' + " " + ap + " " + next_d
+            if day:
+                re = re + ", " + day
+            return re
+
+    if h == 12:
+        if ap == "AM":
+            ap = "PM"
+        else:
+            ap = "AM"
     re = str(h) + " : " + f'{m:02}' + " " + ap
+
     if day:
         re = re + ", " + day
+        return re
 
     return re
 
 
 if __name__ == '__main__':
-    print(add_time("11:30 AM", "2:32", "Monday"))
-# Returns: 2:02 PM, Monday
+    print(add_time("11:43 PM", "24:20", "tueSday"))
+
+# Returns: 12:03 AM, Thursday (2 days later)
